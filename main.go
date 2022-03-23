8b6b657c817608d8f1276d02c947146e81415159
ijann <sugey90210@gmail.com>
2022-03-22 22:04:11 -0300

    definiendo la funcion que lee las plantillas

--- main.go
+++ main.go
@@ -14,7 +14,17 @@ var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
 func loadTemplates() {
-	//TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name + ".html")
+		if (err == nil) {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
 }
 
 func main() {
